8adbec22c2bd18be5c153048f213cb7d4cd00fe1
nbCRAZYMAD <2116913961@qq.com>
2016-07-22 12:59:35 +0800

    2016/7/21 大更新

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,54 +1,15 @@
-#include "vector.h"
 #include <iostream>
-#include <stdio.h>
+#include "vector.h"
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    cjl::vector a;
-    cjl::vector b;
-    int data;
+    cjl::vector<char> a;
+    a.push_back('A');
 
-    printf("请输入a的数据：");
-    for (int i = 0; i < 10; i++)
-    {
-        scanf("%d", &data);
-        a.push_back(data);
-    }
-    printf("请输入b的数据：");
-    for (int i = 0; i < 10; i++)
-    {
-        scanf("%d", &data);
-        b.push_back(data);
-    }
-    cout << "a的数据：";
-    for (int i = 0; i < a.size(); i++)
-    {
-        printf("%d ", a[i]);
-    }
-    cout << endl;
-    cout << "b的数据：";
-    for (int i = 0; i < b.size(); i++)
-    {
-        cout << b[i] << ' ';
-    }
-    a.swap(b);
-    cout << endl << "a、b交换后：" << endl << "a的数据：";
-    for (int i = 0; i < a.size(); i++)
-        cout << a[i] << ' ';
-    cout << endl << "b的数据：";
-    for (int i = 0; i < b.size(); i++)
-        cout << b[i] << ' ';
-    cout << endl;
-    
-    int index;
-    cout << "请输入两个数字，第一个是你要插入的位置，第二个是你要插入的数据:";
-    cin >> index >> data;
-    a.insert(a.begin()+index, data);
-    for (int i = 0; i < a.size(); i++)
-        cout << a[i] << ' ';
-    cout << endl;
+    cout << *find(a.begin(), a.end(), 'A') << endl;
 
     return 0;
 }
--- vector/vector.h
+++ vector/vector.h
@@ -5,23 +5,64 @@
 
 namespace cjl {
 
+template<typename T>
 class vector
 {
 private:
-    int *arry;
-    int maxSize;            // 表明当前数组最大长度
-    int realSize;           // 表名当前数组已容纳的数据量
+    T *arry;
+    T maxSize;            // 表明当前数组最大长度
+    T realSize;           // 表名当前数组已容纳的数据量
     
-    void copyNewArry();
+    void copyNewArry()
+    {
+        maxSize *= 2;
+        T *tmp = new T[maxSize];
+        for (T i = 0; i < realSize; i++)
+        {
+            tmp[i] = arry[i];
+        }
+        delete [] arry;
+        arry = tmp;
+    }
+
 
 public:
-    typedef int* iterator;              // vector是顺序结构，所以将迭代器定义为int*就行了
-    typedef const int* const_iterator;  // 同上
+    typedef T* iterator;              // vector是顺序结构
+    typedef const T* const_iterator;  // 同上
 
-    vector(int length = 100);
-    vector(int data, int size);
-    vector(const vector &tmp);    // 拷贝构造函数
-    ~vector();
+    vector(int length = 100):
+        realSize(0), maxSize(length)
+    {
+        arry = new T[length];
+    }
+
+    vector(T data, int size):
+        maxSize(100), realSize(size)
+    {
+        if (size > maxSize / 2)
+            maxSize = size * 2;
+        arry = new int[maxSize];
+        for (int i = 0; i < size; i++)
+        {
+            arry[i] = data;
+        }
+    }
+
+    vector(const vector &tmp):      // 拷贝构造函数
+        maxSize(tmp.maxSize), realSize(tmp.realSize)
+    {
+        arry = new int[maxSize];
+        for (int i = 0; i < realSize; i++)
+        {
+            arry[i] = tmp.arry[i];
+        }
+    }
+   
+    ~vector()
+    {
+        delete [] arry;
+    }
+
     iterator begin() const
     {
         return arry;
@@ -54,7 +95,13 @@ public:
     {
         return realSize ? false : true;
     }
-    void push_back(int data);   
+    void push_back(int data)   
+    {
+        arry[realSize++] = data;
+        if (realSize == maxSize)
+            copyNewArry();
+    }
+
     void insert(iterator index, int data)       // 从数组中间插入
     {
         if (index == end()) {
@@ -73,8 +120,16 @@ public:
         *(index-1) = data;
         realSize++;
     }
-    void pop_back();
-    int size();
+    void pop_back()
+    {
+        if (realSize == 0)
+            return;
+        realSize--;
+    }
+    int size()
+    {
+        return realSize;
+    }
     int capacity()      // 当前状态下不申请新空间下，能储存的最大数量
     {
         return maxSize;
